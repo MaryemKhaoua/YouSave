8d4a3409707df4c78b0a2069081bed8be24d593e
Maryem Khaoua <maryem.khaoua@gmail.com>
2024-03-17 16:57:09 +0000

    create migration

--- YouSave/database/migrations/2024_03_17_164354_create_blood_types_table.php
+++ YouSave/database/migrations/2024_03_17_164354_create_blood_types_table.php
@@ -11,14 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('blood_types', function (Blueprint $table) {
             $table->id();
-            $table->string('nom');
-            $table->string('prenom');
-            $table->string('tele')->nullable();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean('disponibility')->default(0);
+            $table->string('type');
             $table->timestamps();
         });
     }
@@ -28,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('blood_types');
     }
 };
