dc9d8593c0794b9820fb42269006222e07dbc6a3
Maryem Khaoua <maryem.khaoua@gmail.com>
2024-03-18 22:12:32 +0000

    home page almost done

--- YouSave/resources/views/layouts/main.blade.php
+++ YouSave/resources/views/layouts/main.blade.php
@@ -35,6 +35,34 @@
 
 @yield('content')
 
+<footer class="footer" style="background-color: #dca6a6; padding: 50px 0;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h4>À propos de YouSave</h4>
+                <p>YouSave est une plateforme dédiée au don de sang. Notre mission est de sauver des vies en facilitant le processus de don de sang et en sensibilisant le public à l'importance de ce geste altruiste.</p>
+            </div>
+            <div class="col-md-4">
+                <h4>Contactez-nous</h4>
+                <ul class="list-unstyled">
+                    <li>Adresse: 123 rue YouSave, Ville, Pays</li>
+                    <li>Téléphone: +123456789</li>
+                    <li>Email: contact@yousave.com</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h4>Suivez-nous</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#"><i class="fab fa-facebook-f"></i> Facebook</a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://unpkg.com/gijgo@1.9.14/js/gijgo.min.js" type="text/javascript"></script>
 <link href="https://unpkg.com/gijgo@1.9.14/css/gijgo.min.css" rel="stylesheet" type="text/css"/>
