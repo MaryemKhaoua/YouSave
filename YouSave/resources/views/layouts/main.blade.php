edea268fabec5a2139a046875d1d718e45ffde8b
Maryem Khaoua <maryem.khaoua@gmail.com>
2024-04-23 10:10:46 +0100

    reponsive nav bar

--- YouSave/resources/views/layouts/main.blade.php
+++ YouSave/resources/views/layouts/main.blade.php
@@ -11,32 +11,54 @@
     <link href="/css/style.css" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
 </head>
 <body>
     <header class="navbar navbar-expand-lg navbar-white bg-white shadow sticky-top">
         <div class="container">
             <a class="navbar-brand" href="{{ route('home') }}" style="color: #FF204E;"><span style="color: #A0153E;" class="nav-brand-two">You</span>Save</a>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Rechercher..." aria-label="Search">
-                <button class="btn btn-outline-danger" type="submit"><i class="fas fa-search"></i></button>
-            </form>
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('users.show') }}">List des donneurs</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('apropos.page') }}">A propos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('user.login') }}">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">Register</a>
-                </li>
-            </ul>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <form class="d-flex ms-auto mb-2 mb-lg-0">
+                    <input class="form-control me-2" type="search" id='keySearch' placeholder="Rechercher..." aria-label="Search" onkeyup="search()">
+                    <button class="btn btn-outline-danger" type="submit"><i class="fas fa-search"></i></button>
+                </form>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('users.show') }}">List des donneurs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('apropos.page') }}">A propos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('user.login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                    </li>
+                    @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->prenom}}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="{{ route('user.profil') }}">Profile</a></li>
+                            <li><a class="dropdown-item" href="#">Dashboard</a></li>
+                            <li><a class="dropdown-item" href="{{ route('users.show') }}">List des donneurs</a></li>
+                            <li><a class="dropdown-item" href="#">Fil d'actualité</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="{{ route('logout') }}">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                @endauth
+            </div>
         </div>
     </header>
+
 
     <div class="wrapper">
         @yield('content')
@@ -205,6 +227,11 @@
       </div> --}}
 
     </footer>
+    <script>
+
+
+    </script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-5m2g2I8sH5OZS2wyv4xkW/Zk6oCR5EP6bC+YLZB9nqzDxe/+nvAvCk5QV8KSImBf" crossorigin="anonymous"></script>
